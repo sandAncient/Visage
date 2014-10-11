53fe7be1e8e10431a8954d4abd2e7d96b416d83a
sandancient <sandancient@gmail.com>
2014-10-11 16:53:43 +0800

    first one

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Visage::Application.config.secret_token = '819174a5d91bc611d7ba86be4cf072703da2676cab9596e63d92a7f544b7250c541655feb48e8f76a3bfe5a3898468a77d3fbc12b8f087e32ba01260e827a078'
